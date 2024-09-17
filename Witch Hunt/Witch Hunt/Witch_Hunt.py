c2d8e411bb16c61f4fa84f8258063e6ba31dcdef
Ahmed-BigCreative <ahmed.abdelfattah@bcestudent.com>
2024-09-17 12:05:27 +0100

    i done some

--- Witch Hunt/Witch Hunt/Witch_Hunt.py
+++ Witch Hunt/Witch Hunt/Witch_Hunt.py
@@ -1,1 +1,61 @@
+﻿print("""
+         __________________ _______                               _       _________
+|\     /|\__   __/\__   __/(  ____ \|\     /|  |\     /||\     /|( (    /|\__   __/
+| )   ( |   ) (      ) (   | (    \/| )   ( |  | )   ( || )   ( ||  \  ( |   ) (   
+| | _ | |   | |      | |   | |      | (___) |  | (___) || |   | ||   \ | |   | |   
+| |( )| |   | |      | |   | |      |  ___  |  |  ___  || |   | || (\ \) |   | |   
+| || || |   | |      | |   | |      | (   ) |  | (   ) || |   | || | \   |   | |   
+| () () |___) (___   | |   | (____/\| )   ( |  | )   ( || (___) || )  \  |   | |   
+(_______)\_______/   )_(   (_______/|/     \|  |/     \|(_______)|/    )_)   )_(   
+""")
 
+
+
+from time import sleep
+
+import sys,time
+
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(3./90)
+
+sprint('You awaken...')
+
+sleep(1)
+
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(3./90)
+
+sprint('A dainty figure approaches you, she glows with divinity')
+
+sleep(2)
+
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(3./90)
+
+sprint('You look up.. her face was palewhite like snow. Lined with pearly light clothes she stood atop a shrine glimmering in absoloute authority')
+
+
+sleep(2)
+
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(3./90)
+
+sprint('')
+
+print("Welcome Slayer, My name is Ares")
+
+PlayerName = input("What is your name")
+
+print (PlayerName,"huh, intresting" )
